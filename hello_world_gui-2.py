b4b899031891001abe94f9b96eb61b49c7e0092e
Sidney <sidney.kang@ocsbstudent.ca>
2017-09-12 16:01:30 -0400

    Initial version

---
+++ hello_world_gui-2.py
@@ -0,0 +1,10 @@
+#Created by : Sidney Kang
+#Created on : 12 Sept. 2017
+#Created for : ICS3UR
+# Daily Assignment - Unit0-02
+# This program is the hello, world program, but in GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
