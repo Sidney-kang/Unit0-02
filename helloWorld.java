be35092d12d5d903e6cf214e8e50baf2b1a8b644
Sidney-kang <sidney.kang@ocsbstudent.ca>
2017-10-04 15:45:58 -0400

    Create helloWorld.java
    Initial version

---
+++ helloWorld.java
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *
+ * Created on : 12 Sept. 2017
+ * Created for : ICS3UR
+ * Daily Assignment - Unit0-02
+ * This program is the hello, world program
+ *
+ ****************************************************************************/
+
+public class helloWorld {
+  
+ public static void main(String[] args)
+ {
+  System.out.println("Hello world!");
+ }
+
+}
